87c2d9bdbec92d5f8854f2d969a58744ae7179ef
Bliss <beiwarm@foxmail.com>
2025-04-18 00:44:27 +0800

    add init

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import { useEffect } from 'react'
 function App() {
 
   async function init() {
-    if (!navigator.gpu) {
+    const adapter = await navigator.gpu?.requestAdapter();
+    const device = await adapter?.requestDevice();
+    if (!device) {
       throw Error("WebGPU not supported.");
     }
-
-    const adapter = await navigator.gpu.requestAdapter();
-    if (!adapter) {
-      throw Error("Couldn't request WebGPU adapter.");
-    }
   }
+
 
   useEffect(() => {
     init();
